ff6eb367e5270e7c8df8ef8b8d3a0806a5c67d48
Eltondrknss <eltonoliveira1988@gmail.com>
2025-07-25 10:34:47 -0300

    add comentario fabricas.py

--- fabricas.py
+++ fabricas.py
@@ -3,6 +3,7 @@ from models.arqueiro import Arqueiro
 from models.mago import Mago
 from models.inimigo import Inimigo
 
+# cria uma nova instancia da classe escolhida em main.py
 class FabricaDePersonagens:
     def criar_personagem(self, tipo, nome):
         if tipo == '1':
@@ -13,5 +14,6 @@ class FabricaDePersonagens:
             return Arqueiro(nome, 90, 35, 25)
         elif tipo.lower() == 'inimigo':
             return Inimigo("Jubileu", 100, 30, 20)
-        
+
+        # retorna erro em caso de escolha fora do escopo
         raise ValueError(f"Classe desconhecida: {tipo}")
